b63979a4e12b8a19016b0712e1ae859f36c6c5a2
Nischay Mohan <spectralnischay@hotmail.com>
2025-02-19 03:24:08 -0800

    Pictures load

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
